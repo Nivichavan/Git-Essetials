c31d56a5b21c158aaf0c1ca42bcc0727eccb279f
Nivichavan <nivedita.chavan@niveussolutions.com>
2022-11-29 18:30:14 +0530

    commit

---
+++ SampleProject/src/JUnitPackage/JUnitExample.java
@@ -0,0 +1,44 @@
+package JUnitPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+
+public class JUnitExample {
+
+	
+	    @Test
+	    public void test_JUnit() {
+	        System.out.println("This is the testcase in this class");
+	        String str1="This is the testcase in this class";
+	        assertEquals("This is the testcase in this class", str1);
+	    }
+	    
+	    @Before
+	    public void setUp() {
+	    System.out.println("_______________________________________________________\n");
+	        System.out.println("This is the setUp() method that runs before each testcase");
+	    }
+	    @Test
+	    public void test_JUnit1() {
+	        System.out.println("This is the testcase test_JUnit1() in this class");
+	    }
+	    @Test
+	    public void test_JUnit2() {
+	        System.out.println("This is the testcase test_JUnit2() in this class");
+	    }
+	 
+	    @Test
+	    public void test_JUnit3() {
+	        System.out.println("This is the testcase test_JUnit3() in this class");
+	    }
+	    @After
+	    public void tearDown() {
+	        System.out.println("This is the tearDown() method that runs after each testcase");
+	        System.out.println("_______________________________________________________\n");
+	    }
+
+}
